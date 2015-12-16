13571972892cfe4ddee5f760d78f16645a628007
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-12-15 19:32:26 -0800

    Refactor to use object inheritance

--- pattern.js
+++ pattern.js
@@ -1,5 +1,5 @@
 (function () {
-	var canvas = {
+	var pattern = {
 		w: 800,
 		h: 800,
 		pixelScale: 1,
@@ -12,14 +12,9 @@
 
 		fillPixel: function fillPixel(x, y, colour) {
 			this.ctx.fillStyle = colour;
-			var pc = this.pixelScale;
-			this.ctx.fillRect(x * pc, y * pc, pc, pc);
-		}
-	};
-
-	var map = {
-		size: 0,
-		stick: [],
+			var ps = this.pixelScale;
+			this.ctx.fillRect(x * ps, y * ps, ps, ps);
+		},
 
 		init: function init(size) {
 			this.size = size;
@@ -29,53 +24,54 @@
 			}
 		},
 
+		render: function render(size) {
+			for(var y = 0; y < this.size; y++) {
+				for(var x = 0; x < this.size; x++) {
+					var val = this.getTile(x,y);
+					var colour;
+					if (val === 1) {
+						colour = "#33adff";
+					} else if (val === 0) {
+						colour = "#000000";
+					}
+					this.fillPixel(x, y, colour);
+				}
+			}
+		},
+
 		getTile: function getTile(x, y) {
-			var tileIdx = y * map.size + x;
+			var tileIdx = y * this.size + x;
 			return this.stick[tileIdx];
 		},
 		
 		setTile: function getTile(x, y, val) {
-			var tileIdx = y * map.size + x;
+			var tileIdx = y * this.size + x;
 			return this.stick[tileIdx] = val;
 		}
-	}
+	};
 
-	function drawPattern (map, canvas) {
-		for(var i = 1; i <= map.size; i++) {
-			for(var j = 1; j <= map.size; j++) {
-				var x = i * map.size/100;
-				var y = j * map.size/100;
+	var circular = Object.create(pattern);
+	circular.writePattern = function writePattern() {
+		for(var i = 1; i <= this.size; i++) {
+			for(var j = 1; j <= this.size; j++) {
+				var x = i * this.size/100;
+				var y = j * this.size/100;
 				var c = Math.trunc(x*x + y*y);
 				if (c % 2 === 0) {
-					map.setTile(j, i, 1);
+					this.setTile(j, i, 1);
 				} else {
-					map.setTile(j, i, 0);
+					this.setTile(j, i, 0);
 				}
 			}
 		}
-
-		for(var y = 0; y < map.size; y++) {
-			for(var x = 0; x < map.size; x++) {
-				var val = map.getTile(x,y);
-				var colour;
-				if (val === 1) {
-					colour = "#33adff";
-				} else if (val === 0) {
-					colour = "#000000";
-				}
-				canvas.fillPixel(x, y, colour);
-			}
-		}
-	}
+	};
 
 	var viewWidth = $(window).width();
 	var pixelScale = 1;
-
-	canvas.pixelScale =pixelScale;
-	canvas.h = canvas.w = viewWidth;
-	canvas.build();
-
-	map.init(viewWidth / pixelScale);
-	drawPattern(map, canvas);
-	console.log(map, canvas);
+	pattern.pixelScale =pixelScale;
+	pattern.h = pattern.w = viewWidth;
+	pattern.build();
+	pattern.init(viewWidth / pixelScale);
+	circular.writePattern();
+	circular.render();
 }) ()
