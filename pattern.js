98ac8aeaab57d04695d1745b4f059e757fc2b7fb
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-12-17 21:17:35 -0800

    Use a single point of entry into the procedure

--- pattern.js
+++ pattern.js
@@ -1,45 +1,50 @@
 (function () {
 	var pattern = {
-		build: function build() {
-			var canvas = $("<canvas width='" + this.w + "' height='" + this.h + "'" + "class='" + "pattern'>" + "</canvas>");
+		viewWidth: $(window).width(),
+		pixelScale: 1,
+		colourA: "#FFFFFF",
+		colourB: "#000000",
+		render: function render() {
+			this.initTiles(this.viewWidth / this.pixelScale);
+			this.writePattern();
+			this.attachCanvas();
+			this.draw();
+		},
+		attachCanvas: function attachCanvas() {
+			var canvas = $("<canvas width='" + this.viewWidth + "' height='" + this.viewWidth + "'" + "class='" + "pattern'>" + "</canvas>");
 			this.ctx = canvas.get(0).getContext("2d");
 			canvas.appendTo($("body"));
 		},
-
 		fillPixel: function fillPixel(x, y, colour) {
 			this.ctx.fillStyle = colour;
 			var ps = this.pixelScale;
 			this.ctx.fillRect(x * ps, y * ps, ps, ps);
 		},
-
-		init: function init(size) {
+		initTiles: function initTiles(size) {
 			this.size = size;
 			this.stick = []
 			for(var i = 0; i < size * size; i++) {
 				this.stick.push(0);
 			}
 		},
-
-		render: function render(size) {
+		draw: function () {
 			for(var y = 0; y < this.size; y++) {
 				for(var x = 0; x < this.size; x++) {
 					var val = this.getTile(x,y);
 					var colour;
 					if (val === 1) {
-						colour = "#33adff";
+						colour = this.colourA;
 					} else if (val === 0) {
-						colour = "#000000";
+						colour = this.colourB;
 					}
 					this.fillPixel(x, y, colour);
 				}
 			}
 		},
-
 		getTile: function getTile(x, y) {
 			var tileIdx = y * this.size + x;
 			return this.stick[tileIdx];
 		},
-		
 		setTile: function getTile(x, y, val) {
 			var tileIdx = y * this.size + x;
 			return this.stick[tileIdx] = val;
@@ -62,12 +67,8 @@
 		}
 	};
 
-	var viewWidth = $(window).width();
-	var pixelScale = 1;
-	pattern.pixelScale =pixelScale;
-	pattern.h = pattern.w = viewWidth;
-	pattern.build();
-	pattern.init(viewWidth / pixelScale);
-	circular.writePattern();
+	circular.pixelScale = 1;
+	circular.colourA = "#33adff";
+	circular.colourB = "#000000";
 	circular.render();
-}) ()
+})()
